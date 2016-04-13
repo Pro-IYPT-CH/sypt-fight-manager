520265e8cc5e1a5c94fadd675049b3ab2dae37c4
Sven G <halibali@gmx.ch>
2016-04-13 13:37:37 +0200

    Added all files

---
+++ phases.js
@@ -0,0 +1,57 @@
+var phases = [
+	{
+		"title": "Presentation by the Reporter",
+		"duration": 720,
+		"startingtime": 0
+	},
+	{
+		"title": "Clarifying questions of the Opponent to the Reporter",
+		"duration": 120,
+		"startingtime": 720
+	},
+	{
+		"title": "Preparation of the Opponent",
+		"duration": 180,
+		"startingtime": 840
+	},
+	{
+		"title": "Presentation of the Opposition",
+		"duration": 300,
+		"startingtime": 1020
+	},
+	{
+		"title": "Discussion between Opponent and Reporter",
+		"duration": 360,
+		"startingtime": 1320
+	},
+	{
+		"title": "Summary of the discussion by the Opponent",
+		"duration": 60,
+		"startingtime": 1680
+	},
+	{
+		"title": "Clarifying questions of the Reviewer to Reporter and Opponent",
+		"duration": 180,
+		"startingtime": 1740
+	},
+	{
+		"title": "Preparation of the Reviewer",
+		"duration": 120,
+		"startingtime": 1920
+	},
+	{
+		"title": "Presentation of the Review",
+		"duration": 240,
+		"startingtime": 2040
+	},
+	{
+		"title": "Concluding remarks by the Reporter",
+		"duration": 120,
+		"startingtime": 2280
+	},
+	{
+		"title": "Questions of the Jury to all three teams",
+		"duration": 300,
+		"startingtime": 2400
+	}
+];
