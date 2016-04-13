39f616f1a0efd5372cd5d2d6127e640ae3e87ac9
AedanChristie <aedan@thechristies.ch>
2016-04-13 16:42:07 +0200

    Added Thorax, Semantic UI and Mockups

--- js/main.js
+++ js/main.js
@@ -1,6 +1,121 @@
 var maxPhaseId = 10;
 var interval;
 var timer = 0;
+var phases = [
+	{
+		"title": "Presentation by the Reporter",
+		"duration": 720,
+		"startingtime": 0,
+    "addTimeToNext": false
+	},
+	{
+		"title": "Clarifying questions of the Opponent to the Reporter",
+		"duration": 120,
+		"startingtime": 720,
+    "addTimeToNext": false
+	},
+	{
+		"title": "Preparation of the Opponent",
+		"duration": 180,
+		"startingtime": 840,
+    "addTimeToNext": false
+	},
+	{
+		"title": "Presentation of the Opposition",
+		"duration": 300,
+		"startingtime": 1020,
+    "addTimeToNext": true
+	},
+	{
+		"title": "Discussion between Opponent and Reporter",
+		"duration": 360,
+		"startingtime": 1320,
+    "addTimeToNext": false
+	},
+	{
+		"title": "Summary of the discussion by the Opponent",
+		"duration": 60,
+		"startingtime": 1680,
+    "addTimeToNext": false
+	},
+	{
+		"title": "Clarifying questions of the Reviewer to Reporter and Opponent",
+		"duration": 180,
+		"startingtime": 1740,
+    "addTimeToNext": false
+	},
+	{
+		"title": "Preparation of the Reviewer",
+		"duration": 120,
+		"startingtime": 1920,
+    "addTimeToNext": false
+	},
+	{
+		"title": "Presentation of the Review",
+		"duration": 240,
+		"startingtime": 2040,
+    "addTimeToNext": false
+	},
+	{
+		"title": "Concluding remarks by the Reporter",
+		"duration": 120,
+		"startingtime": 2280,
+    "addTimeToNext": false
+	},
+	{
+		"title": "Questions of the Jury to all three teams",
+		"duration": 300,
+		"startingtime": 2400,
+    "addTimeToNext": false
+	}
+];
+
+var fight = [
+  {
+    title: "Stage 1",
+    reporter: "John Smith",
+    opponent: "Bob Dylan",
+    reviewer: "Jane Doe",
+    phases: phases
+  },
+  {
+    title: "Stage 2",
+    opponent: "John Smith",
+    reporter: "Bob Dylan",
+    reviewer: "Jane Doe",
+    phases: phases
+  },
+  {
+    title: "Stage 3",
+    reviewer: "John Smith",
+    opponent: "Bob Dylan",
+    reporter: "Jane Doe",
+    phases: phases
+  }
+];
+
+Handlebars.templates["app"] = Handlebars.compile(document.getElementById("app-template"));
+var App = Thorax.View.extend({
+  name: "app"
+
+});
+
+App.initClock = function () {
+
+};
+
+App.pause = function () {
+
+};
+
+App.startFight = function (fight) {
+  this._fight = fight;
+  this.currentStage = 0;
+  this.currentPhase = 0;
+  this.initClock();
+};
+
+
 function startFight(){
   if((gV("fightInput") != "") && (gV("stageInput") != "")){
     el("startButtonDiv").style.display = "none";
